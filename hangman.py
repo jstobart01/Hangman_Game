5b0fe497236723d8b39e4dff440a7bca12305a57
jstobart01 <stobartjackson@gmail.com>
2023-11-25 09:20:17 -0800

    Attempted to refactor the function - needs improve

--- hangman.py
+++ hangman.py
@@ -1,22 +1,34 @@
 # Hangman game
 # Author: Jackson Stobart
-# Rev 1.0 - 2023-11-23
+# Rev 1.1 - 2023-11-25
 
 print("Welcome to my Python Hangman Game!")
 
 # Test word for initial game play
 word = "apple"
 letter = ""
+player_guess = []
 # print("".join(word))
 
 letter = input("\nPlease guess a letter: ")
 # print(letter)
 
-def letter_guess(letter, word):
-    if letter in word:
-        print("correct")
-    else:
-        print("incorrect")
-    
-letter_guess(letter, word)
 
+def letter_guess(letter, word):
+    if player_string != word:
+        if letter in word:
+            global player_guess
+            player_guess.append(letter)
+            print(player_guess)
+            player_string = str(player_guess)
+        else:
+            print("incorrect") 
+    else:
+        print("Congrats")
+
+#player_string = str(player_guess)
+
+#if player_string == word:
+   # print("correct word")
+# else:
+    
---
+++ hangman_launch.py
@@ -0,0 +1,3 @@
+# This will be the file that the hangman game is launched from
+
+from hangman import letter_guess
